542aa012f05e73e25f8af9a0dd236efd6b7cd345
Heldson Luiz Silva <heldsonluiz@gmail.com>
2023-06-26 22:00:24 -0300

    :sparkles: Integração com Stripe

---
+++ src/lib/stripe.ts
@@ -0,0 +1,9 @@
+import Stripe from "stripe";
+
+const apiKey: string = process.env.STRIPE_SECRET_KEY || ''
+export const stripe = new Stripe(apiKey,{
+  apiVersion: "2022-11-15",
+  appInfo: {
+    name: 'Ignite Shop'
+  }
+});
